16cf5bf6b187946e436a73935e5a1d46c48433ea
Sho Kodama <madakodama.1go@gmail.com>
2015-06-20 11:27:09 +0900

    newメソッド作成　パーシャルでformを作る　newにrenderでフォームを呼び出す

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,15 +3,20 @@ class CustomersController < ApplicationController
   end
 
   def new
+    @customer = Customer.new
   end
 
   def create
+    @customer = Customer.find(:id)
+    @customer.save
+    redirect_to
   end
 
   def edit
   end
 
   def update
+    @customer = Customer
   end
 
   def show
