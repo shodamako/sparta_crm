9821db9296b22de75463ff6acc385ca373cccbe0
Sho Kodama <madakodama.1go@gmail.com>
2015-06-20 16:35:00 +0900

    seedを編集 rand(3) + 1は、０以上３未満の数字に１を足して切り捨てなので、＋１をする

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,12 @@
   Customer.create(
     family_name: Faker::Japanese::Name.last_name,
     given_name: Faker::Japanese::Name.first_name,
-    email: "customer_#{index}@sparta.com"
+    email: "customer_#{index}@sparta.com",
+    company_id: rand(3) + 1
   )
 end
+
+Company.create(name: "スパルタ商事", url: "http://spartacamp.jp/", address: "東京都千代田区１−１−１")
+Company.create(name: "株式会社ノーウォール", url: "http://nowall.co.jp/", address: "東京都新宿区西新宿6-15-1")
+Company.create(name: "ヤフー株式会社", url: "http://www.yahoo.co.jp/", address: "東京都港区赤坂9-7-1")
 
